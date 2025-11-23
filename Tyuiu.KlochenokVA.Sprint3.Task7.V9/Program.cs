dafe679dc513b33a2ee60683d85a00d2b038cd1f
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-24 02:52:27 +0500

    7

---
+++ Tyuiu.KlochenokVA.Sprint3.Task7.V9/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.KlochenokVA.Sprint3.Task7.V9.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint3.Task7.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Клоченок В. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                *");
+            Console.WriteLine("* Задание #7                                                             *");
+            Console.WriteLine("* Вариант #9                                                             *");
+            Console.WriteLine("* Выполнил: Клоченок Владимир Алексеевич | ИСПб-25-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции          *");
+            Console.WriteLine("* F(x) = (2*x-3)/(cos(x)+x) + 5 на заданном диапазоне [-5; 5]          *");
+            Console.WriteLine("* с шагом 1. Произвести проверку деления на ноль.                       *");
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            string strLine;
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+
+            double[] valueArray = ds.GetMassFunction(startValue, stopValue);
+            for (int i = 0; i <= stopValue - startValue; i++)
+            {
+                strLine = String.Format("|{0,5:d}     | {1, 5:f2}    |", startValue, valueArray[i]);
+                Console.WriteLine(strLine);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
