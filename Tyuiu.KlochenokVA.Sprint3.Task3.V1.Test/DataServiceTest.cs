c3fd6c62ba1eea834f93352121f4d2396f8cb303
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-24 02:09:22 +0500

    task3

---
+++ Tyuiu.KlochenokVA.Sprint3.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KlochenokVA.Sprint3.Task3.V1.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint3.Task3.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCharCount()
+        {
+            DataService ds = new DataService();
+
+            string value = "have a nice time";
+            char item = 'a';
+
+            int res = ds.GetCharCount(value, item);
+            int wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KlochenokVA.Sprint3.Task3.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KlochenokVA.Sprint3.Task3.V1.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint3.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Клоченок В. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                           *");
+            Console.WriteLine("* Задание #3                                                             *");
+            Console.WriteLine("* Вариант #1                                                             *");
+            Console.WriteLine("* Выполнил: Клоченок Владимир Алексеевич | ИСПб-25-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Используя цикл foreach подсчитать количество символов 'a' в строке     *");
+            Console.WriteLine("* have a nice time                                                       *");
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            string value = "have a nice time";
+            char item = 'a';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Искомый символ = " + item);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Количество символов = " + ds.GetCharCount(value, item));
+
+            Console.ReadKey();
+        }
+    }
+}
