d31fd6bc550672ebc7ce12bc2c01110d35650bd4
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-24 01:14:21 +0500

    Task5

---
+++ Tyuiu.KlochenokVA.Sprint3.Task5.V29.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KlochenokVA.Sprint3.Task5.V29.Lib
+{
+    public class DataService : ISprint3Task5V29
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries = sumSeries + (Math.Pow(x, k) + Math.Sin(k));
+                }
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
